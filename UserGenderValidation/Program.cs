c1d68791a1164a0e8fdb860214ea9a523b5352a4
Karl <karl.oliver.sepp@tktk.ee>
2023-02-15 19:01:35 +0200

    add week2 code

---
+++ UserGenderValidation/Program.cs
@@ -0,0 +1,26 @@
+﻿// rakendus küsib sugu
+//rakendus küsib perekonna nime
+//lähtudest infost tervitab kasutajat
+//"Welcome, Mr/Ms (perekonnanimi)"
+
+
+
+Console.WriteLine("Please, select your gender (m/f)");
+
+char userGender = Char.Parse(Console.ReadLine());//Loeb konsoolist maha andmeid string (sõne) formaadis
+Console.WriteLine("Please, enter your last name:");
+
+string userLastName = Console.ReadLine();
+
+if (userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr. {userLastName}!");
+}
+else if (userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms. {userLastName}!");
+}
+else
+{
+    Console.WriteLine($"Welcome, {userLastName}!");
+} 
